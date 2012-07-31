70f8f8ee792de76773059bfc3e00503d7f39e937
Kyle Laker <10kylelaker@gmail.com>
2012-07-31 01:10:39 -0400

    Change from MV to CP

--- ADB-Install-Mac.sh
+++ ADB-Install-Mac.sh
@@ -8,18 +8,19 @@
 #  very soon.
 #
 #  Created by kalaker on 04/20/2012
-#  Updated by kalaker on 05/30/2012
+#  Updated by kalaker on 07/31/2012
 #  http://forum.xda-developers.com/member.php?u=4510103 <-My XDA Profile
+#  https://github.com/teamblueridge/adb-fastboot-install <-GitHub Page
 echo "This will install ADB and Fastboot on your computer."
 echo "Root Permissions required. Please type your password."
 sudo cd ~ #Gives Superuser permissions
 cd "`dirname "$0"`" #CDs to script directory
 echo "Changed directory to `pwd`" #Informs user of path change
 echo "Moving ADB"
-sudo mv Mac/adb_Mac /usr/bin/adb #Moves adb
+sudo cp Mac/adb_Mac /usr/bin/adb #Moves adb
 echo "ADB Moved to /usr/bin/adb"
 echo "moving Fastboot"
-sudo mv Mac/fastboot_Mac /usr/bin/fastboot #Moves Fastboot
+sudo cp Mac/fastboot_Mac /usr/bin/fastboot #Moves Fastboot
 echo "Fastboot moved to /usr/bin/fastboot"
 echo "You may now run Android Debug Bridge and Fastboot commands"
 echo "Have a nice day."
