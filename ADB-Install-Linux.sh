bee4c6042a44d6753bf5acbe902af531fc29c97f
Kyle Laker <10kylelaker@gmail.com>
2014-04-03 22:12:33 -0400

    Update adb version; install to /usr/local/bin

--- ADB-Install-Linux.sh
+++ ADB-Install-Linux.sh
@@ -2,31 +2,33 @@
 
 #  ADB Install.sh
 #  
-#  This script installs adb and fastboot into usr/bin/
+#  This script installs adb and fastboot into usr/local/bin/
 #  This may be run from anywhere and is compatible with Linux
 #  and Mac OS X. Hopefully a Windows batch file will be available
 #  very soon.
 #
 #  Created by kalaker on 04/20/2012
-#  Updated by kalaker on 07/31/2012
+#  Updated by kalaker on 04/03/2014
 #  http://forum.xda-developers.com/member.php?u=4510103 <-My XDA Profile
 #  https://github.com/teamblueridge/adb-fastboot-install <-GitHub Page
+install_path=/usr/local/bin
 echo "This will install ADB and Fastboot on your computer."
 echo "Root Permissions required. Please type your password."
 sudo cd ~ #Gives Superuser permissions
 cd "`dirname "$0"`" #CDs to script directory
 echo "Installing JDK (Not really necessary, but helpful for future)."
-sudo add-apt-repository ppa:sun-java-community-team/sun-java6; sudo apt-get update; sudo apt-get install sun-java6-jre sun-java6-bin sun-java6-jdk
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository ppa:webupd8team/java; sudo apt-get update; sudo apt-get install oracle-java6-installer
 echo "JDK Installed"
 echo "Changed directory to `pwd`" #Informs user of path change
 echo "Moving ADB"
-sudo cp Linux/adb_Linux /usr/bin/adb #Moves adb
-sudo chmod 777 /usr/bin/adb #Allows to be executable
-echo "ADB Moved to /usr/bin/adb"
+sudo cp Linux/adb_Linux $install_path/adb #Moves adb
+sudo chmod 777 $install_path/adb #Allows to be executable
+echo "ADB Moved to $install_path/adb"
 echo "moving Fastboot"
-sudo cp Linux/fastboot_Linux /usr/bin/fastboot #Moves Fastboot
-sudo chmod 777 /usr/bin/fastboot #Allows to be executable
-echo "Fastboot moved to /usr/bin/fastboot"
+sudo cp Linux/fastboot_Linux $install_path/fastboot #Moves Fastboot
+sudo chmod 777 $install_path/fastboot #Allows to be executable
+echo "Fastboot moved to $install_path/fastboot"
 echo "Installing rules"
 sudo cp Linux/51-android.rules /etc/udev/rules.d/51-android.rules #Installs rules
 echo "Rules installed"
--- ADB-Install-Mac.sh
+++ ADB-Install-Mac.sh
@@ -2,25 +2,26 @@
 
 #  ADB Install.sh
 #  
-#  This script installs adb and fastboot into /usr/bin/
+#  This script installs adb and fastboot into usr/local/bin/
 #  This may be run from anywhere and is compatible with Linux
 #  and Mac OS X. Hopefully a Windows batch file will be available
 #  very soon.
 #
 #  Created by kalaker on 04/20/2012
-#  Updated by kalaker on 07/31/2012
+#  Updated by kalaker on 04/03/2014
 #  http://forum.xda-developers.com/member.php?u=4510103 <-My XDA Profile
 #  https://github.com/teamblueridge/adb-fastboot-install <-GitHub Page
+install_path=/usr/local/bin
 echo "This will install ADB and Fastboot on your computer."
 echo "Root Permissions required. Please type your password."
 sudo cd ~ #Gives Superuser permissions
 cd "`dirname "$0"`" #CDs to script directory
 echo "Changed directory to `pwd`" #Informs user of path change
 echo "Moving ADB"
-sudo cp Mac/adb_Mac /usr/bin/adb #Moves adb
-echo "ADB Moved to /usr/bin/adb"
+sudo cp Mac/adb_Mac $install_path/adb #Moves adb
+echo "ADB Moved to $install_path/adb"
 echo "moving Fastboot"
-sudo cp Mac/fastboot_Mac /usr/bin/fastboot #Moves Fastboot
-echo "Fastboot moved to /usr/bin/fastboot"
+sudo cp Mac/fastboot_Mac $install_path/fastboot #Moves Fastboot
+echo "Fastboot moved to $install_path/fastboot"
 echo "You may now run Android Debug Bridge and Fastboot commands"
 echo "Have a nice day."
